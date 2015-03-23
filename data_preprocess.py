57e6801e855078dde56d618b96d2fe91e2ddce9b
bkpathak <bkpathak@mtu.edu>
2015-03-22 17:34:52 -0700

    added level to distunguish two same secondary

--- data_preprocess.py
+++ data_preprocess.py
@@ -5,13 +5,17 @@ import csv
 with open('levelwise_enrollment.csv', 'r') as csvinput:
     header = next(csvinput).strip() + ",Gender"
     print header
+    count = 0
     for row in csvinput:
+        count += 1
         field = row.strip().split(",")
         levelGender= field[1].split(" ")
-        field[1] = levelGender[0]
-        if levelGender[1] == "Girls":
+        if count % 12 == 0 or count % 12 == 11:
+            field[1] = " ".join(levelGender[0:-1]) + " level"
+        else:
+            field[1] = " ".join(levelGender[0:-1])
+        if levelGender[-1] == "Girls":
             field.append("F")
         else:
             field.append("M")
         print ','.join(field)
-
